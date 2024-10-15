d43365900eab84de0233aea29065b70dd657b59e
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-10-14 21:29:14 -0700

    SayHello function is applied only to valid moves, and clearMoves function will remove the previously listed event listener from any moves that are no longer valid

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let board = [
     [0, 0, 0],
     [1, 1, 1]
 ];
+let outerBoard = document.getElementById('outer-board');
 
 function playGame() {
     // user will make moves until they win or are defeated by the 'ai'
 };
 
 function showBoard(board) {
-    let outerBoard = document.getElementById('outer-board');
     // transform rows into array to get key-value pairs from entries
     for(const [rowIndex, row] of [...outerBoard.children].entries()) {
         // transform columns into array to get key-value pairs from entries
@@ -17,16 +17,13 @@ function showBoard(board) {
             if(board[rowIndex][colIndex] === -1 && !col.firstChild) {
                 let item = document.createElement('img');
                 item.src = 'img/black-pawn.png';
-                item.addEventListener('click', () => {
-                    moveForward(rowIndex, colIndex);
-                });
                 col.appendChild(item);
             } else if (board[rowIndex][colIndex] === 1 && !col.firstChild) {
                 let item = document.createElement('img');
                 item.src = 'img/white-pawn.png';
                 // event listener shows coordinates of pawn
                 item.addEventListener('click', () => {
-                    moveForward(rowIndex, colIndex);
+                    showMoves(rowIndex, colIndex);
                 });
                 col.appendChild(item);
             } else if (board[rowIndex][colIndex] === 0 && col.firstChild) {
@@ -37,6 +34,41 @@ function showBoard(board) {
             col.appendChild(testEle);*/
             /*console.log(board[rowIndex][colIndex]);
             console.log(col);*/
+        };
+    };
+};
+
+
+function showMoves(row, column) {
+    // calls clearMoves function to remove colors and event listeners
+    clearMoves();
+    // if the spot in front of it doesnt have a pawn, highlight it for move
+    if(row && board[row - 1][column] === 0) {
+        let newRow = ([...outerBoard.children][row - 1]);
+        let newSpace = ([...newRow.children][column]);
+        console.log(newSpace);
+        newSpace.style.backgroundColor = "yellow";
+        newSpace.addEventListener('click', sayHello);
+        console.log("Move forward available");
+    };
+    // if the spot to the left/right does have a pawn, highlight it for attack
+    if(row && board[row - 1][column - 1] === -1) {
+        console.log("Attack left available");
+    }
+    if (row && board[row - 1][column + 1] === -1) {
+        console.log("Attack right available");
+    }
+};
+
+function sayHello() {
+    console.log("Heyo");
+};
+
+function clearMoves() {
+    for(const [rowIndex, row] of [...outerBoard.children].entries()) {
+        for(const [colIndex, col] of [...row.children].entries()) {
+            col.removeEventListener('click', sayHello);
+            col.style.backgroundColor = "transparent";
         };
     };
 };
