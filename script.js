edb8391a4716e070dc64352177da2bf3d97ab6d0
Aosegir <128326345+Aosegir@users.noreply.github.com>
2024-10-14 20:48:05 -0700

    White board pieces are now able to be moved and will not enter an illegal space

--- script.js
+++ script.js
@@ -14,21 +14,42 @@ function showBoard(board) {
     for(const [rowIndex, row] of [...outerBoard.children].entries()) {
         // transform columns into array to get key-value pairs from entries
         for(const [colIndex, col] of [...row.children].entries()) {
-            if(board[rowIndex][colIndex] === -1) {
+            if(board[rowIndex][colIndex] === -1 && !col.firstChild) {
                 let item = document.createElement('img');
                 item.src = 'img/black-pawn.png';
+                item.addEventListener('click', () => {
+                    moveForward(rowIndex, colIndex);
+                });
                 col.appendChild(item);
-            } else if (board[rowIndex][colIndex] === 1) {
+            } else if (board[rowIndex][colIndex] === 1 && !col.firstChild) {
                 let item = document.createElement('img');
                 item.src = 'img/white-pawn.png';
+                // event listener shows coordinates of pawn
+                item.addEventListener('click', () => {
+                    moveForward(rowIndex, colIndex);
+                });
                 col.appendChild(item);
-            }
+            } else if (board[rowIndex][colIndex] === 0 && col.firstChild) {
+                col.removeChild(col.firstChild);
+            };
             /*const testEle = document.createElement('h1');
             testEle.innerHTML = board[rowIndex][colIndex];
             col.appendChild(testEle);*/
             /*console.log(board[rowIndex][colIndex]);
             console.log(col);*/
         };
+    };
+};
+
+function moveForward(row, column) {
+    console.log(board[row][column]);
+    // if statement checks to see if there is a row above it and if the row is empty
+    // before entering code
+    if(row && board[row - 1][column] === 0) {
+        board[row - 1][column] = 1;
+        board[row][column] = 0;
+        console.log(board);
+        showBoard(board);
     };
 };
 
